05b68ca4b364feced42aed8fb28b1b2c3ad58b57
Ramon Valk <RamonValk@MacBook-Pro-van-Ramon.local>
2016-10-05 10:24:55 +0200

    no message

---
+++ public/config.js
@@ -0,0 +1,5 @@
+window.PiLab = window.PiLab || {};
+window.PiLab.config = {
+  firebaseUrl: 'https://device-lab-df354.firebaseio.com/',
+  firebaseKey: 'AIzaSyB2pR5-24usk1vtE1lMXMD03tX36vueT-k'
+};
---
+++ public/scripts/main.js
@@ -0,0 +1,86 @@
+/* jshint browser: true */
+/* global Firebase, fbKey */
+'use strict';
+
+var primaryPanel = document.querySelector('primary-panel');
+var pages = document.querySelector('core-animated-pages');
+var pToast = document.querySelector('paper-toast');
+var pErrorToast = document.querySelector('error-toast');
+var ignoreError = true;
+var PiLabMode;
+
+
+/*var fb = new Firebase(PiLab.config.firebaseUrl);
+Oude inlogmethode
+*/
+
+var config = {
+    apiKey: "AIzaSyB2pR5-24usk1vtE1lMXMD03tX36vueT-k",
+    authDomain: "device-lab-df354.firebaseapp.com",
+    databaseURL: "https://device-lab-df354.firebaseio.com",
+    storageBucket: "device-lab-df354.appspot.com",
+    messagingSenderId: "25225191200"
+  };
+  firebase.initializeApp(config);
+
+var fb = firebase.database().ref();
+
+
+fb.auth.signInWithCustomToken(PiLab.config.firebaseKey, function(error) {
+  if(error) {
+    console.error('[FIREBASE] Auth failed. ' + error.toString());
+    window.showErrorToast('Firebase authentication failure.');
+  } else {
+    console.log('[FIREBASE] Auth success.');
+    fb.child('.info/connected').on('value', function(snapshot) {
+      if (snapshot.val() === true) {
+        window.showToast('Firebase connected.');
+      } else {
+        window.showErrorToast('Network disconnected.');
+      }
+    });
+    fb.child('config/useMode').on('value', function(snapshot) {
+      PiLabMode = snapshot.val();
+    });
+  }
+});
+
+
+// firebase.auth().signInWithCustomToken(customToken).catch(function(error) {
+//   if(error) {
+//     console.error('[FIREBASE] Auth failed. ' + error.toString());
+//     window.showErrorToast('Firebase authentication failure.');
+//   } else {
+//     console.log('[FIREBASE] Auth success.');
+//     fb.child('.info/connected').on('value', function(snapshot) {
+//       if (snapshot.val() === true) {
+//         window.showToast('Firebase connected.');
+//       } else {
+//         window.showErrorToast('Network disconnected.');
+//       }
+//     });
+//     fb.child('config/useMode').on('value', function(snapshot) {
+//       PiLabMode = snapshot.val();
+//     });
+//   }
+// });
+
+
+window.showErrorToast = function(message) {
+  if (pToast.opened === true) {
+    pToast.dismiss();
+  }
+  if (message.length > 60) {
+    message = message.substring(0, 59) + '...';
+  }
+  pErrorToast.text = message;
+  pErrorToast.show();
+  if (navigator.vibrate) {
+    navigator.vibrate([300, 100, 100]);
+  }
+};
+
+window.showToast = function(message) {
+  pToast.text = message;
+  pToast.show();
+};
